8d7e39fce65cb78df348a3bc00ec79c395c1ad2c
HongC03 <86474083+HongC03@users.noreply.github.com>
2021-08-20 15:01:39 +0800

    Testing for react API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Basic } from './Basic';
+import { Airtable } from './Airtable';
+
+function App() {
+	return (
+		<React.Fragment>
+			<Basic />
+      <Airtable />
+		</React.Fragment>
+	);
+}
+
+export default App;

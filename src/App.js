00c9cd8f9d378666337f254b3d978e21913042de
HongC03 <86474083+HongC03@users.noreply.github.com>
2021-08-20 16:08:07 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Basic } from './Basic';
 import { Airtable } from './Airtable';
 
 function App() {
 	return (
 		<React.Fragment>
-			<Basic />
       <Airtable />
 		</React.Fragment>
 	);
